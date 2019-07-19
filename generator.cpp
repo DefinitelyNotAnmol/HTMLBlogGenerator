aeeeace292aba080553f87e3b1ce43896b155f13
Anmol Jain <itsmeaj20@gmail.com>
2019-07-19 14:04:06 +0530

    Added first documentation [more elements to be added]

---
+++ generator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
+
+/*
+Title - Required
+/Title
+    Format for title
+/
+
+Short Title - Required
+/STitle
+    Short title 
+/
+
+Date - Required
+/Date
+    Feb 07, 2019
+/
+/Date Auto/  **This will generate automatic date, according to the date of compilation of the file**
+
+MainImage - Required [1200 x 800]
+/MImage
+    image.jpg
+/
+
+Paragraph - Optional
+/Para
+    This is the format for paragraph text
+/
+
+BlockQuote - Optional
+/Block
+    This is the block quote text
+/
+ */
