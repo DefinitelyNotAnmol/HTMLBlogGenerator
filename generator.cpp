d0152b9b68bbe6363ec3e67ead969c19b1690ed0
Anmol Jain <itsmeaj20@gmail.com>
2019-09-14 19:06:03 +0530

    Completed Documentation

--- generator.cpp
+++ generator.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+#include "constants.h"
 
 int main(int argc, char *argv[]) {
 
+    if (argc != 2) {
+        std::cout << "Please check filenames" << std::endl;
+        return 1;
+    }
+
+    constants::blog blog = constants::generate(argv[1]);
+    std::ofstream outfile;
+    outfile.open(blog.STitle + ".html", std::ios::out | std::ios::trunc);
+    outfile << blog.Data;
+    outfile.close();
     return 0;
 }
+
+/*
+    Save an file using tags mentioned below and save the file with extension .admd
+    also keep all the images in the same directory as .admd file
+
+    after running the program copy image folder to <blogName> folder to images/Blog folder of website
+    copy <blogName>.html to static/blog folder in website
+*/
 
 /*
 Title - Required
