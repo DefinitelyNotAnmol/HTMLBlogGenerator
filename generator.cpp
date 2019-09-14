c2561a0b671dfaec25c83dab7323d241c163d3f6
Anmol Jain <itsmeaj20@gmail.com>
2019-09-14 17:52:04 +0530

    Updated tags

--- generator.cpp
+++ generator.cpp
@@ -23,8 +23,18 @@ Date - Required
 /
 /Date Auto/  **This will generate automatic date, according to the date of compilation of the file**
 
+Tag - Required
+/Tag
+    Design Resources, Adobe Illustrator
+/
+
 MainImage - Required [1200 x 800]
 /MImage
+    image.jpg
+/
+
+Image - Optional
+/Image
     image.jpg
 /
 
@@ -38,5 +48,31 @@ BlockQuote - Optional
     This is the block quote text
 /
 
-/Strong /
+Bullets - Optional
+/Bullets
+    firstline
+    second line
+/
+
+NumberedList - Optional
+/List
+    firstline
+    secondline
+/
+
+Bold - Optional
+/Bold
+    line
+/
+
+External Links - Optional
+/Link 
+    <text>
+    <link>
+/
+
+Author - Required
+/Sign
+    Anmol Jain
+/
  */
