c33b723af41e953f04354b37cfa701011dae6149
Anmol Jain <itsmeaj20@gmail.com>
2019-09-29 11:23:36 +0530

    updated the filename error handling

--- generator.cpp
+++ generator.cpp
@@ -4,11 +4,17 @@
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 2) {
-        std::cout << "Please check filenames" << std::endl;
+    if (argc == 1) {
+        std::cout << "no input file" << std::endl;
         return 1;
     }
 
+    if (argc > 2) {
+        std::cout << "Filename is not correct, please make sure you use \" \" if you have spaces in the file name " << std::endl;
+        return 2;
+    }
+
     admd::bloggify(argv[1]);
+
     return 0;
 }
